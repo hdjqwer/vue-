b9ef7443400eeb9f39889a656f47e5df1fdf9707
hdjqwer <1786032425@qq.com>
2018-01-13 00:09:31 +0800

    集成

---
+++ src/component/login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        登录界面
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                a:10,
+                b:50
+            }
+        },
+        created () {
+            this.$http.get(this.$api.isLogin).then(res =>{
+                
+            })
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 import App from './component/App.vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
+import App from './component/App.vue';
+import routerConfig from './router/index.js';
+import axios from './js/api-config.js';
+Vue.prototype.$api = api;
 new Vue ({
     el:'#app',
     render:function(createElement){
         return createElement(App);
-    }
+    },
+    
+      router: new VueRouter(routerConfig)
+
 })
