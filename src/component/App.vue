8ce646ff80efced14d224e0ba4b92488645d409b
hdjqwer <1786032425@qq.com>
2018-01-13 10:36:53 +0800

    引用element-ui

--- src/component/App.vue
+++ src/component/App.vue
@@ -3,13 +3,18 @@
        小花花送给你
         <router-view></router-view>
         <!-- 根组件会有一些子组件, 我把这些子分为两种: 一种是子页面, 一种是页面布局结构 -->      
-      
+        <el-rate v-model="value" :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+        </el-rate>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data(){
+            return {
+                value:3
+            };
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import elementui from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
-
+Vue.use(elementui);
 import App from './component/App.vue';
 import routerConfig from './router/index.js';
 import axios from 'axios';
