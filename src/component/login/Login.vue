9e5adda13d24a2e39f339a29bd0271827acbd3ea
hdjqwer <1786032425@qq.com>
2018-01-13 09:59:16 +0800

    提交5

--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -13,8 +13,10 @@
         },
         created () {
             this.$http.get(this.$api.isLogin).then(res =>{
+                console.log(res.data);
                 
             })
         }
     }
 </script>
+<script scoped></script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
-import App from './component/App.vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import App from './component/App.vue';
 import routerConfig from './router/router.js';
-import axios from './js/api-config.js';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://157.122.54.189:9095';
+Vue.prototype.$api = api;
+Vue.prototype.$http =axios;
+
+import api from './js/api-config';
 Vue.prototype.$api = api;
 new Vue ({
     el:'#app',
