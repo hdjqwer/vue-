2786ca66cc011d7b1716f5c9438f5f5b8c962815
hdjqwer <1786032425@qq.com>
2018-01-13 10:04:20 +0800

    7

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import LoginComponent from '../component/login/Login.vue';
 import RegisterComponent from '../component/register.vue';
 export default {
     router: [
-        {name:"h",path:"/home",component:HomeComponent},
         {name:"1",path:"/login",component:LoginComponent},
- 
+        {name:"r",path:"/register",component:RegisterComponent},
     ]
 }
